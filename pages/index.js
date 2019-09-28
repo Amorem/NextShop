9d918b17c1483b9b0c72acd72bcbedf735385703
AMOREM <dev@amorem.com>
2019-09-28 09:03:15 +0200

    Fetching darta from the server using getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import axios from "axios";
 
-function Home() {
-  React.useEffect(() => {
-    getProducts();
-  }, []);
-
-  async function getProducts() {
-    const url = "http://localhost:3000/api/products";
-    const response = await axios.get(url);
-  }
+function Home({ products }) {
+  console.log(products);
 
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  // Fetch data on server
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  // Return response as an object
+  return { products: response.data };
+  // This object will be merged into existing props
+};
 
 export default Home;
