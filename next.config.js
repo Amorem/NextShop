30bc8324351a3dda3cb4dadb346c3db962ec6443
AMOREM <dev@amorem.com>
2019-09-29 18:58:33 +0200

    User model + Signup user (hash + token + cookie)

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     MONGO_SRV:
       "mongodb+srv://amorem:Foforum@cluster0-a8boz.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "zddcsvzvvoejpojfzecoihezfoiscd",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/amorem/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
