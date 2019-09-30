14d7144a68802eb95d5abb5bbf06dc32cc861648
AMOREM <dev@amorem.com>
2019-09-30 07:17:54 +0200

    Get currentUser from token, protect auth Routes

--- utils/auth.js
+++ utils/auth.js
@@ -5,3 +5,15 @@ export function handleLogin(token) {
   cookie.set("token", token);
   Router.push("/account");
 }
+
+export function redirectUser(ctx, location) {
+  // Redirect if we are on the server
+  if (ctx.req) {
+    ctx.res.writeHead(302, { Location: location });
+    ctx.req.end();
+  }
+  // Redirect if we are on the client
+  else {
+    Router.push(location);
+  }
+}
