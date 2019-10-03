4a0a5c9d8c44254f8ed6283ee487dc1f667f6fa1
AMOREM <dev@amorem.com>
2019-10-03 22:35:01 +0200

    Orders history

--- components/Account/AccountOrders.js
+++ components/Account/AccountOrders.js
@@ -1,5 +1,89 @@
-function AccountOrders() {
-  return <>AccountOrders</>;
+import { useRouter } from "next/router";
+import {
+  Accordion,
+  Button,
+  Header,
+  Icon,
+  Image,
+  Label,
+  List,
+  Segment
+} from "semantic-ui-react";
+
+function AccountOrders({ orders }) {
+  const router = useRouter();
+
+  function mapOrdersToPanels(orders) {
+    return orders.map(order => ({
+      key: order._id,
+      title: {
+        content: <Label color="blue" content={order.createdAt} />
+      },
+      content: {
+        content: (
+          <>
+            <List.Header as="h3">
+              Total: ${order.total}
+              <Label
+                content={order.email}
+                icon="mail"
+                basic
+                horizontal
+                style={{ marginLeft: "1em" }}
+              ></Label>
+            </List.Header>
+            <List>
+              {order.products.map(p => (
+                <List.Item>
+                  <Image avatar src={p.product.mediaUrl} />
+                  <List.Content>
+                    <List.Header>{p.product.name}</List.Header>
+                    <List.Description>
+                      {p.quantity} x {p.product.price}
+                    </List.Description>
+                  </List.Content>
+                  <List.Content floated="right">
+                    <Label tag color="red" size="tiny">
+                      {p.product.sku}
+                    </Label>
+                  </List.Content>
+                </List.Item>
+              ))}
+            </List>
+          </>
+        )
+      }
+    }));
+  }
+
+  return (
+    <>
+      <Header as="h2">
+        <Icon name="folder open" />
+        Order History
+      </Header>
+      {orders.length === 0 ? (
+        <Segment inverted tertiary color="grey" textAlign="center">
+          <Header icon>
+            <Icon name="copy outline" />
+            No past orders
+          </Header>
+          <div>
+            <Button color="orange" onClick={() => router.push("/")}>
+              View products
+            </Button>
+          </div>
+        </Segment>
+      ) : (
+        <Accordion
+          fluid
+          styled
+          exclusive={false}
+          panels={mapOrdersToPanels(orders)}
+        />
+      )}
+    </>
+  );
 }
 
 export default AccountOrders;
