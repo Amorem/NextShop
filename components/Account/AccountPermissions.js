4613bd892b7d3f8cda45e9325a9203af6cd16a7c
AMOREM <dev@amorem.com>
2019-10-04 10:45:50 +0200

    Updating user permissions in DB

--- components/Account/AccountPermissions.js
+++ components/Account/AccountPermissions.js
@@ -46,16 +46,37 @@ function AccountPermissions() {
 }
 
 function UserPermission({ user }) {
+  const [admin, setAdmin] = React.useState(user.role === "admin");
+  const isFirstRun = React.useRef(true);
+
+  React.useEffect(() => {
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      return;
+    }
+    updatePermissions();
+  }, [admin]);
+
+  function handleChangePermission() {
+    setAdmin(previousState => !previousState);
+  }
+
+  async function updatePermissions() {
+    const url = `${baseUrl}/api/account`;
+    const payload = { _id: user._id, role: admin ? "admin" : "user" };
+    await axios.put(url, payload);
+  }
+
   return (
     <Table.Row>
       <Table.Cell collapsing>
-        <Checkbox toggle />
+        <Checkbox checked={admin} toggle onChange={handleChangePermission} />
       </Table.Cell>
       <Table.Cell>{user.name}</Table.Cell>
       <Table.Cell>{user.email}</Table.Cell>
       <Table.Cell>{user.createdAt}</Table.Cell>
       <Table.Cell>{user.updatedAt}</Table.Cell>
-      <Table.Cell>{user.role}</Table.Cell>
+      <Table.Cell>{admin ? "admin" : "user"}</Table.Cell>
     </Table.Row>
   );
 }
